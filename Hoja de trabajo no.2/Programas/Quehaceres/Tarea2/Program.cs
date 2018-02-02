ac911f70683edfde71d7bcd3f1bd51c3c7e7d7e5
Alan rueda <rueda171067@unis.edu.gt>
2018-02-01 19:06:37 -0600

    Tarea no. 2

---
+++ Hoja de trabajo no.2/Programas/Quehaceres/Tarea2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona Persona1 = new Persona();
+            Persona Persona2 = new Persona();
+
+            QueHacer queHacer1= new QueHacer("Lavar Platos", new DateTime(), "Lavar los platos en la cocina");
+            QueHacer queHacer2 = new QueHacer("Barrer el segundo nivel", new DateTime(), "Barrer el piso de la casa");
+            QueHacer queHacer3 = new QueHacer("Trapear el segundo nivel", new DateTime(), "Trapear el piso del segundo nivel de la casa");
+            QueHacer queHacer4 = new QueHacer("Arreglar las habitaciones", new DateTime(), "Arreglar las habitaciones del primer y segundo nivel");
+            QueHacer queHacer5 = new QueHacer("Lavar los baños", new DateTime(), "Lavar los baños de la casa");
+            
+
+            Persona1.AgregarQueHacer(queHacer1);
+            Persona1.AgregarQueHacer(queHacer2);
+            Persona1.AgregarQueHacer(queHacer3);
+
+            Persona2.AgregarQueHacer(queHacer4);
+            Persona2.AgregarQueHacer(queHacer5);
+            
+
+            Persona1.CompletarQueHacer();
+
+            Persona2.CompletarQueHacer();
+            Persona2.CompletarQueHacer();
+            Persona2.CompletarQueHacer();
+
+            Console.WriteLine("Persona uno esta disponible? => " + Persona1.EstaDisponible());
+            Console.WriteLine("Persona dos esta disponible? => " + Persona2.EstaDisponible());
+            Console.ReadLine();
+        }
+    }
+}
