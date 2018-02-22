3ef4a9741556872b94b4c1779df429f97fdfe1c8
Alan rueda <rueda171067@unis.edu.gt>
2018-02-22 17:38:33 -0600

    Tarea no. 4

---
+++ Hoja de trabajo no. 4/Programs/Genericos/Genericos.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genericos
+
+{
+
+    public class Genericos
+
+    {
+
+        public Genericos() { }
+
+
+        public T Head<T>(T[] lista)
+
+        {
+
+            return lista[0];
+
+        }
+
+        public T[] Tail<T>(T[] lista)
+
+        {
+
+            T[] nuevaLista = new T[lista.Length - 1];
+
+            int comodin = 0;
+
+            for (int i = 1; i < lista.Length; i++)
+
+            {
+
+                nuevaLista[comodin] = lista[i];
+
+                comodin += 1;
+
+            }
+
+            return nuevaLista;
+
+        }
+
+        public Tupla<T1, T2>[] Zip<T1, T2>(T1[] listaA, T2[] listaB)
+
+        {
+
+            Tupla<T1, T2>[] arreglo = new Tupla<T1, T2>[Longitud<T1, T2>(listaA, listaB)];
+
+
+            for (int i = 0; i < arreglo.Length; i++)
+
+            {
+
+                arreglo[i] = new Tupla<T1, T2>(listaA[i], listaB[i]);
+
+            }
+
+            return arreglo;
+
+        }
+
+        private int Longitud<T1, T2>(T1[] valorA, T2[] valorB)
+
+        {
+
+            int longitud = 0;
+
+            if (valorA.Length == valorB.Length)
+
+            {
+
+                longitud = valorA.Length;
+
+            }
+
+            else if (valorA.Length > valorB.Length)
+
+            {
+
+                longitud = valorB.Length;
+
+            }
+
+            else if (valorA.Length < valorB.Length)
+
+            {
+
+                longitud = valorA.Length;
+
+            }
+
+            return longitud;
+
+        }
+
+    }
+
+}
---
+++ Hoja de trabajo no. 4/Programs/GenericosTests/GenericosTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericosTest
+
+{
+
+    public class GenericosTest
+
+    {
+        public object Assert { get; private set; }
+
+        public void HeadTest()
+
+        {
+
+            int[] arreglo = { 1, 2, 3 };
+
+            Genericos generico = new Genericos();
+
+            int valor = generico.Head<int>(arreglo);
+
+            Assert.AreEqual(valor, arreglo[0]);
+
+        }
+
+        public void TailTest()
+        {
+
+            int[] ArregloBase = { 10, 20, 30, 40 };
+
+            Genericos generico = new Generico();
+
+            int[] NuevoArreglo = genericos.Tail<int>(ArregloBase);
+
+            Assert.AreEqual(ArregloBase[0], NuevoArreglo[0]);
+
+        }
+
+        public void TestZip()
+        {
+
+            Genericos genericos = new Genericos();
+
+            int[] a = { 1, 2, 3, 4 };
+
+            int[] b = { 1, 2, 3, 4 };
+
+            Tupla<int, int>[] arreglo = genericos.Zip<int, int>(a, b);
+
+
+
+            for (int i = 0; i < arreglo.Length; i++)
+
+            {
+
+                Assert.AreEqual(a[i], arreglo[i].primero);
+
+                Assert.AreEqual(b[i], arreglo[i].segundo);
+
+            }
+
+        }
+
+    }
+
+}
