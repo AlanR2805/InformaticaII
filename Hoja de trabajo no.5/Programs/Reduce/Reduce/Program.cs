51e775580862ea9ff2f1a4e1f031f92e6b6e1d5a
Alan rueda <rueda171067@unis.edu.gt>
2018-03-01 22:16:19 -0600

    tarea no. 5

---
+++ Hoja de trabajo no.5/Programs/Reduce/Reduce/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reduce
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int ReduceInt(int[] array, int primero, Func<int, int, int> reduce)
+        {
+            int acumulador = primero;
+
+            for (int a = 0; a < array.Length; a++)
+            {
+                acumulador = reduce(acumulador, array[a]);
+            }
+
+            return acumulador;
+        }
+
+        public static A Reduce<T, A> (T[] array, A primero, Func<A, T, A> reduce)
+        {
+            A acumulador = primero;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                acumulador = reduce(acumulador, array[i]);
+            }
+
+            return acumulador;
+        }
+
+    }
+}
---
+++ Hoja de trabajo no.5/Programs/Reduce/ReduceTest/ReduceTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Reduce;
+using System.Linq;
+
+namespace ReduceTest
+{
+    [TestClass]
+    public class ReduceTest
+    {
+        [TestMethod]
+        public void ReduceIntTest()
+        {
+        
+            int[] valores = new int[] { 2, 8, 9 };
+            int reduce = Program.ReduceInt(valores, 0, (acc, valor) => acc + valor);
+
+            Assert.AreEqual(reduce, 20);
+        }
+
+        [TestMethod]
+        public void ReduceGenericTest()
+        {
+            //Suma la cantidad de veces que aparece la letra a en los elementos del arreglo
+            string[] valores = new string[] { "Buenas", "tardes", "espero ganar" };
+            int reduce = Program.Reduce(valores, 0, (acc, valor) => acc + valor.Where(c => c == 'a').Count());
+
+            Assert.AreEqual(reduce, 10);
+        }
+    }
+}
