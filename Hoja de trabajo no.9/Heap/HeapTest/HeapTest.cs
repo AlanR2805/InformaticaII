2a9c190643c6b09ccb266810c52dcaccb500ba93
Alan rueda <rueda171067@unis.edu.gt>
2018-05-08 20:03:47 -0600

    Hoja de trabajo no. 9

---
+++ Hoja de trabajo no.9/Heap/HeapTest/HeapTest.cs
@@ -0,0 +1,17 @@
+﻿using Heap;
+using System;
+using Xunit;
+
+namespace HeapTest
+{
+	public class HeapTest
+	{
+		[Fact]
+		public void HeapfyTest()
+		{
+			int[] arreglo = { 3, 8, 6, 4, 21, 12 };
+			Program.Heapify(arreglo);
+			Assert.Equal(new int[] { 21, 12, 8, 6, 4, 3 }, arreglo);			
+		}
+	}
+}
